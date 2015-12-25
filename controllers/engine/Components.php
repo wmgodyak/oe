82dcd6f5132839e06cf7283895f67177699b44da
Volodymyr Godyak <wmgodyak@gmail.com>
2015-12-25 17:53:41 +0200

    Створив компонент користувачі і компоненти

---
+++ controllers/engine/Components.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * OYiEngine 7
+ * @author Volodymyr Hodiak mailto:support@otakoi.com
+ * @copyright Copyright (c) 2015 Otakoyi.com
+ * Date: 25.12.15 : 17:46
+ */
+
+namespace controllers\engine;
+
+use controllers\Engine;
+
+/**
+ * Class Components
+ * @name Компоненти
+ * @ion fa fa-puzzle-piece
+ * @package controllers\engine
+ */
+class Components extends Engine
+{
+    public function index()
+    {
+        // TODO: Implement index() method.
+    }
+    public function create()
+    {
+        // TODO: Implement create() method.
+    }
+    public function edit($id)
+    {
+        // TODO: Implement edit() method.
+    }
+    public function delete($id)
+    {
+        // TODO: Implement delete() method.
+    }
+    public function process($id)
+    {
+        // TODO: Implement process() method.
+    }
+}
