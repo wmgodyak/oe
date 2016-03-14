09f35fbf0ab0756b3240cea443a9ba7d27bd0b48
Volodymyr Godyak <wmgodyak@gmail.com>
2016-03-14 15:25:21 +0200

    ContentImagesSizes Resize OK

---
+++ helpers/Image.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * OYiEngine 7
+ * @author Volodymyr Hodiak mailto:support@otakoi.com
+ * @copyright Copyright (c) 2015 Otakoyi.com
+ * Date: 14.03.16 : 14:53
+ */
+
+
+namespace helpers;
+
+
+defined("CPATH") or die();
+
+class Image
+{
+
+}
