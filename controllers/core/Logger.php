2be5d0ed9a653393c3bd4b725b67323a6ad872f8
Volodymyr Godyak <wmgodyak@gmail.com>
2016-03-31 09:48:57 +0300

    Add Logger

---
+++ controllers/core/Logger.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * OYiEngine 7
+ * @author Volodymyr Hodiak mailto:support@otakoi.com
+ * @copyright Copyright (c) 2015 Otakoyi.com
+ * Date: 31.03.16 : 9:37
+ */
+
+namespace controllers\core;
+
+defined("CPATH") or die();
+
+class Logger
+{
+    private static $root = 'logs/';
+    private static $filename;
+
+    /**
+     * @param $dir
+     * @param $filename
+     * @param string $ext
+     */
+    public static function init($dir, $filename = '', $ext = 'log')
+    {
+        $path = self::$root . $dir . '/';
+
+        if(!is_dir(DOCROOT . $path)) mkdir(DOCROOT . $path, 0777, true);
+
+        if(!empty($filename)) {
+            $filename = preg_replace('^[a-z0-9]','', $filename);
+            $filename .= '-';
+        }
+
+        self::$filename = DOCROOT . $path . $filename . date('Y-m-d') . '.' . $ext;
+    }
+
+    /**
+     * @param $text
+     * @return int
+     */
+    public static function log($text)
+    {
+        $text = date('c') . '   ' . $text . "\r\n";
+
+        return file_put_contents(self::$filename , $text, FILE_APPEND);
+    }
+}
---
+++ models/app/Comments.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * OYiEngine 7
+ * @author Volodymyr Hodiak mailto:support@otakoi.com
+ * @copyright Copyright (c) 2015 Otakoyi.com
+ * Date: 30.03.16 : 14:36
+ */
+
+
+namespace models\app;
+
+
+defined("CPATH") or die();
+
+class Comments
+{
+
+}
