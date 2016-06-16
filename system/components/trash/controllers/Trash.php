040e7ba6f7a352f406f3d0941fe442eb09126cd1
Wolodymyr Hodiak <wmgodyak@gmail.com>
2016-06-16 20:38:24 +0300

    Trash component

--- system/components/trash/controllers/Trash.php
+++ system/components/trash/controllers/Trash.php
@@ -1,31 +1,102 @@
 <?php
 
-namespace system\components\blank\controllers;
+namespace system\components\trash\controllers;
 
+use helpers\bootstrap\Button;
+use helpers\bootstrap\Icon;
+use system\core\DataTables2;
 use system\Engine;
 
 defined("CPATH") or die();
 
-class Blank extends Engine
+/**
+ * Class Trash
+ * @package system\components\blank\controllers
+ */
+class Trash extends Engine
 {
+    private $trash;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->trash = new \system\components\trash\models\Trash();
+    }
+
     public function index()
     {
-        // TODO: Implement index() method.
+        $t = new DataTables2('content');
+
+        $t  -> orderDef(0, 'desc')
+            -> th($this->t('common.id'), 'c.id', 1,1, 'width:60px')
+            -> th($this->t('common.name'), 'ci.name', 1,1)
+            -> th($this->t('common.created'), 'c.created', 1,1)
+            -> th($this->t('common.updated'), 'c.updated', 1,1)
+            -> th($this->t('common.tbl_func'), null, null, null, 'width: 160px')
+
+            -> get('ci.url')
+            -> get('c.status')
+            -> get('c.isfolder')
+            -> get('CONCAT(u.name, \' \' , u.surname) as owner')
+
+            -> ajax('trash/items')
+        ;
+
+        $this->output($t->init());
     }
-    public function create()
+
+    public function items()
     {
-        // TODO: Implement create() method.
+        $t = new DataTables2();
+        $t  -> from('__content c')
+            -> join("__content_info ci on ci.content_id=c.id and ci.languages_id={$this->languages_id}")
+            -> join('__users u on u.id=c.owner_id')
+            -> where(" c.status = 'deleted' ")
+            -> execute();
+
+//            -> get('ci.url, c.status, c.isfolder, CONCAT(u.name, \' \' , u.surname) as owner')
+        $res = array();
+        foreach ($t->getResults(false) as $i=>$row) {
+            $icon = Icon::create(($row['isfolder'] ? 'fa-folder' : 'fa-file'));
+            $status = $this->t('trash.status_' . $row['status']);
+            $res[$i][] = $row['id'];
+            $res[$i][] =
+                " <a class='status-{$row['status']}' title='{$status}' href='trash/index/{$row['id']}'>{$icon}  {$row['name']}</a>"
+                . "<br><small class='label label-info'>Автор:{$row['owner']} </small>"
+            ;
+            $res[$i][] = date('d.m.Y H:i:s', strtotime($row['created']));
+            $res[$i][] = $row['updated'] ? date('d.m.Y H:i:s', strtotime($row['updated'])) : '';
+            $res[$i][] =
+                (string)Button::create
+                (
+                    Icon::create(Icon::TYPE_RESTORE),
+                    ['class' => 'b-trash-restore btn-primary', 'data-id' => $row['id'], 'title' => $this->t('trash.restore_question')]
+                )
+                .
+                (string)Button::create
+                (
+                    Icon::create(Icon::TYPE_DELETE),
+                    ['class' => 'b-trash-remove btn-danger', 'data-id' => $row['id'], 'title' => $this->t('trash.remove_question')]
+                )
+            ;
+        }
+
+        return $t->render($res, $t->getTotal());
     }
-    public function edit($id)
+
+    public function remove($id)
     {
-        // TODO: Implement edit() method.
+        $this->trash->remove($id);
     }
-    public function delete($id)
+
+    public function restore($id)
     {
-        // TODO: Implement delete() method.
+        $this->trash->restore($id);
     }
-    public function process($id)
-    {
-        // TODO: Implement process() method.
-    }
+
+    public function create(){}
+    public function edit($id){}
+    public function delete($id){}
+    public function process($id){}
 }
--- system/components/trash/models/Trash.php
+++ system/components/trash/models/Trash.php
@@ -6,10 +6,53 @@
  * Time: 20:27
  */
 
-namespace system\components\trash;
+namespace system\components\trash\models;
 
+use system\models\Engine;
 
-class Trash
+defined("CPATH") or die();
+
+/**
+ * Class Trash
+ * @package system\components\trash\models
+ */
+class Trash extends Engine
 {
+    /**
+     * @param $id
+     * @param string $key
+     * @return array|mixed
+     * @throws \system\core\exceptions\Exception
+     */
+    private function getData($id, $key = '*')
+    {
+        return self::$db->select("select {$key} from __content where id={$id} limit 1")->row($key);
+    }
 
+    /**
+     * @param $id
+     * @throws \system\core\exceptions\Exception
+     */
+    public function remove($id)
+    {
+        $s = self::$db->delete("__content", "id={$id} limit 1");
+        if($s){
+            self::$db->delete("__content_features", "content_id={$id}");
+            self::$db->delete("__features_content", "content_id={$id}");
+        }
+    }
+
+    /**
+     * @param $id
+     */
+    public function restore($id)
+    {
+        $parent_id = $this->getData($id, 'parent_id');
+
+        $s = parent::updateRow('__content', $id, ['status' => 'published']);
+
+        if($s > 0 && $parent_id > 0){
+            parent::updateRow('__content', $parent_id, ['isfolder' => 1]);
+        }
+    }
 }
