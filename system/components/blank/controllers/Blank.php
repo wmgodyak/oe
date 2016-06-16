232370f1b02b7481fbc25183cf7b8ddf14d1c129
Wolodymyr Hodiak <wmgodyak@gmail.com>
2016-06-16 19:00:54 +0300

    System settings OK

---
+++ system/components/blank/controllers/Blank.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace system\components\blank\controllers;
+
+use system\Engine;
+
+defined("CPATH") or die();
+
+class Blank extends Engine
+{
+    public function index()
+    {
+        // TODO: Implement index() method.
+    }
+    public function create()
+    {
+        // TODO: Implement create() method.
+    }
+    public function edit($id)
+    {
+        // TODO: Implement edit() method.
+    }
+    public function delete($id)
+    {
+        // TODO: Implement delete() method.
+    }
+    public function process($id)
+    {
+        // TODO: Implement process() method.
+    }
+}
---
+++ system/components/settings/models/Settings.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wg
+ * Date: 16.06.16
+ * Time: 17:22
+ */
+
+namespace system\components\settings\models;
+use system\models\Model;
+
+defined("CPATH") or die();
+
+class Settings extends Model
+{
+    public function get()
+    {
+        $r = self::$db->select("select * from __settings")->all();
+        $res = [];
+        foreach ($r as $item) {
+            $res[$item['block']][$item['id']] = $item;
+        }
+        return $res;
+    }
+
+    public function update()
+    {
+        $settings = $this->request->post('settings');
+        foreach ($settings as $name=>$value) {
+            \system\models\Settings::getInstance()->set($name, $value);
+        }
+
+        $this->updateRobotsTxt($settings);
+    }
+
+    private function updateRobotsTxt($settings)
+    {
+        if(!isset($settings['robots_index_sample'])) return;
+
+        if($settings['site_index'] == 1){
+            $text = $settings['robots_index_sample'];
+            $text = str_replace(['{app}','{appurl}'],[APP, APPURL], $text);
+        } else{
+            $text = $settings['robots_no_index_sample'];
+        }
+
+        file_put_contents(DOCROOT . 'robots.txt', $text);
+    }
+
+    public function getVersion()
+    {
+        return self::$db->query('select version()')->fetchColumn();
+    }
+}
