d094ed57271be06f0bbbe50aec4407e1a70e5e32
Wolodymyr Hodiak <wmgodyak@gmail.com>
2016-06-23 23:13:43 +0300

    Створив blank модуль-заготовку

---
+++ modules/blank/controllers/Blank.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wg
+ * Date: 23.06.16
+ * Time: 23:11
+ */
+
+namespace modules\blank\controllers;
+
+use system\Front;
+
+class Blank extends Front
+{
+
+}
---
+++ modules/blank/models/Blank.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wg
+ * Date: 23.06.16
+ * Time: 23:12
+ */
+
+namespace modules\blank\models;
+
+use system\models\Model;
+
+class Blank extends Model
+{
+
+}
