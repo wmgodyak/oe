40be5f1320d48d40d3ba7786538c430948a91e69
Wolodymyr Hodiak <wmgodyak@gmail.com>
2016-09-02 15:12:49 +0300

    clear

--- changes.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE  `e_orders_status_history` CHANGE  `id`  `id` INT( 11 ) NOT NULL AUTO_INCREMENT ;
-DROP TABLE `e_mail_templates`, `e_mail_templates_info`;
-ALTER TABLE `e_content` ADD `quantity` TINYINT(3) UNSIGNED NULL DEFAULT NULL AFTER `unit_id`, ADD INDEX (`quantity`);
