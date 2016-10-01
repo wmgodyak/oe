d69e3bd0f47f2db277a5efb61f40d96cbe2f947c
Wolodymyr Hodiak <wmgodyak@gmail.com>
2016-10-01 22:40:58 +0300

    Shop nav order fix

--- modules/shop/models/Categories.php
+++ modules/shop/models/Categories.php
@@ -30,6 +30,7 @@ class Categories extends Content
           join __content_types ct on ct.type = '{$this->type}' and ct.id=c.types_id
           join __content_info ci on ci.content_id=c.id and ci.languages_id='{$this->languages_id}'
           where c.parent_id='{$parent_id}' and c.status = 'published'
+          order by abs(c.position) asc
           ")->all();
 
         if($level > 0){
