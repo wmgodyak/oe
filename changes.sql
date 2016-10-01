cee52e8305d278a6e0e11448b9c6372aa21d466b
Wolodymyr Hodiak <wmgodyak@gmail.com>
2016-10-01 10:22:49 +0300

    Add icon twitter and ok to footer

--- changes.sql
+++ changes.sql
@@ -1,6 +1,3 @@
-ALTER TABLE  `e_features_content` ADD INDEX (  `content_id` ) ;
-ALTER TABLE  `e_currency` ADD INDEX (  `id` ) ;
-
 delete from e_content_meta where content_id not in (select id from e_content);
 delete from e_content_info where content_id not in (select id from e_content);
 delete from e_products_prices where content_id not in (select id from e_content);
