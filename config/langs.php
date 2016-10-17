ccfb8671e414e68c25583ac0bbe7a11e8dc84972
Volodymyr Godyak <wmgodyak@gmail.com>
2016-10-17 17:16:16 +0300

    move langs to config

---
+++ config/langs.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * OYiEngine 7
+ * @author Volodymyr Hodiak mailto:support@otakoi.com
+ * @copyright Copyright (c) 2015 Otakoyi.com
+ * Date: 17.10.16 : 17:05
+ */
+
+defined("CPATH") or die();
+
+return [
+    'en' => 'English',
+    'uk' => 'Українська',
+    'ru' => 'Русский'
+];
--- system/core/Lang.php
+++ system/core/Lang.php
@@ -23,193 +23,6 @@ class Lang
     private $dir;
     private $lang = null;
 
-    private $_langs = [
-        'aa' => 'Afar',
-        'ab' => 'Abkhaz',
-        'ae' => 'Avestan',
-        'af' => 'Afrikaans',
-        'ak' => 'Akan',
-        'am' => 'Amharic',
-        'an' => 'Aragonese',
-        'ar' => 'Arabic',
-        'as' => 'Assamese',
-        'av' => 'Avaric',
-        'ay' => 'Aymara',
-        'az' => 'Azerbaijani',
-        'ba' => 'Bashkir',
-        'be' => 'Belarusian',
-        'bg' => 'Bulgarian',
-        'bh' => 'Bihari',
-        'bi' => 'Bislama',
-        'bm' => 'Bambara',
-        'bn' => 'Bengali',
-        'bo' => 'Tibetan Standard, Tibetan, Central',
-        'br' => 'Breton',
-        'bs' => 'Bosnian',
-        'ca' => 'Catalan; Valencian',
-        'ce' => 'Chechen',
-        'ch' => 'Chamorro',
-        'co' => 'Corsican',
-        'cr' => 'Cree',
-        'cs' => 'Czech',
-        'cu' => 'Old Church Slavonic, Church Slavic, Church Slavonic, Old Bulgarian, Old Slavonic',
-        'cv' => 'Chuvash',
-        'cy' => 'Welsh',
-        'da' => 'Danish',
-        'de' => 'German',
-        'dv' => 'Divehi; Dhivehi; Maldivian;',
-        'dz' => 'Dzongkha',
-        'ee' => 'Ewe',
-        'el' => 'Greek, Modern',
-        'en' => 'English',
-        'eo' => 'Esperanto',
-        'es' => 'Spanish; Castilian',
-        'et' => 'Estonian',
-        'eu' => 'Basque',
-        'fa' => 'Persian',
-        'ff' => 'Fula; Fulah; Pulaar; Pular',
-        'fi' => 'Finnish',
-        'fj' => 'Fijian',
-        'fo' => 'Faroese',
-        'fr' => 'French',
-        'fy' => 'Western Frisian',
-        'ga' => 'Irish',
-        'gd' => 'Scottish Gaelic; Gaelic',
-        'gl' => 'Galician',
-        'gn' => 'GuaranÃ­',
-        'gu' => 'Gujarati',
-        'gv' => 'Manx',
-        'ha' => 'Hausa',
-        'he' => 'Hebrew (modern)',
-        'hi' => 'Hindi',
-        'ho' => 'Hiri Motu',
-        'hr' => 'Croatian',
-        'ht' => 'Haitian; Haitian Creole',
-        'hu' => 'Hungarian',
-        'hy' => 'Armenian',
-        'hz' => 'Herero',
-        'ia' => 'Interlingua',
-        'id' => 'Indonesian',
-        'ie' => 'Interlingue',
-        'ig' => 'Igbo',
-        'ii' => 'Nuosu',
-        'ik' => 'Inupiaq',
-        'io' => 'Ido',
-        'is' => 'Icelandic',
-        'it' => 'Italian',
-        'iu' => 'Inuktitut',
-        'ja' => 'Japanese (ja)',
-        'jv' => 'Javanese (jv)',
-        'ka' => 'Georgian',
-        'kg' => 'Kongo',
-        'ki' => 'Kikuyu, Gikuyu',
-        'kj' => 'Kwanyama, Kuanyama',
-        'kk' => 'Kazakh',
-        'kl' => 'Kalaallisut, Greenlandic',
-        'km' => 'Khmer',
-        'kn' => 'Kannada',
-        'ko' => 'Korean',
-        'kr' => 'Kanuri',
-        'ks' => 'Kashmiri',
-        'ku' => 'Kurdish',
-        'kv' => 'Komi',
-        'kw' => 'Cornish',
-        'ky' => 'Kirghiz, Kyrgyz',
-        'la' => 'Latin',
-        'lb' => 'Luxembourgish, Letzeburgesch',
-        'lg' => 'Luganda',
-        'li' => 'Limburgish, Limburgan, Limburger',
-        'ln' => 'Lingala',
-        'lo' => 'Lao',
-        'lt' => 'Lithuanian',
-        'lu' => 'Luba-Katanga',
-        'lv' => 'Latvian',
-        'mg' => 'Malagasy',
-        'mh' => 'Marshallese',
-        'mi' => 'Maori',
-        'mk' => 'Macedonian',
-        'ml' => 'Malayalam',
-        'mn' => 'Mongolian',
-        'mr' => 'Marathi (Mara?hi)',
-        'ms' => 'Malay',
-        'mt' => 'Maltese',
-        'my' => 'Burmese',
-        'na' => 'Nauru',
-        'nb' => 'Norwegian BokmÃ¥l',
-        'nd' => 'North Ndebele',
-        'ne' => 'Nepali',
-        'ng' => 'Ndonga',
-        'nl' => 'Dutch',
-        'nn' => 'Norwegian Nynorsk',
-        'no' => 'Norwegian',
-        'nr' => 'South Ndebele',
-        'nv' => 'Navajo, Navaho',
-        'ny' => 'Chichewa; Chewa; Nyanja',
-        'oc' => 'Occitan',
-        'oj' => 'Ojibwe, Ojibwa',
-        'om' => 'Oromo',
-        'or' => 'Oriya',
-        'os' => 'Ossetian, Ossetic',
-        'pa' => 'Panjabi, Punjabi',
-        'pi' => 'Pali',
-        'pl' => 'Polska',
-        'ps' => 'Pashto, Pushto',
-        'pt' => 'Portuguese',
-        'qu' => 'Quechua',
-        'rm' => 'Romansh',
-        'rn' => 'Kirundi',
-        'ro' => 'Romanian, Moldavian, Moldovan',
-        'ru' => 'Русский',
-        'rw' => 'Kinyarwanda',
-        'sa' => 'Sanskrit (Sa?sk?ta)',
-        'sc' => 'Sardinian',
-        'sd' => 'Sindhi',
-        'se' => 'Northern Sami',
-        'sg' => 'Sango',
-        'si' => 'Sinhala, Sinhalese',
-        'sk' => 'Slovak',
-        'sl' => 'Slovene',
-        'sm' => 'Samoan',
-        'sn' => 'Shona',
-        'so' => 'Somali',
-        'sq' => 'Albanian',
-        'sr' => 'Serbian',
-        'ss' => 'Swati',
-        'st' => 'Southern Sotho',
-        'su' => 'Sundanese',
-        'sv' => 'Swedish',
-        'sw' => 'Swahili',
-        'ta' => 'Tamil',
-        'te' => 'Telugu',
-        'tg' => 'Tajik',
-        'th' => 'Thai',
-        'ti' => 'Tigrinya',
-        'tk' => 'Turkmen',
-        'tl' => 'Tagalog',
-        'tn' => 'Tswana',
-        'to' => 'Tonga (Tonga Islands)',
-        'tr' => 'Turkish',
-        'ts' => 'Tsonga',
-        'tt' => 'Tatar',
-        'tw' => 'Twi',
-        'ty' => 'Tahitian',
-        'ug' => 'Uighur, Uyghur',
-        'uk' => 'Українська',
-        'ur' => 'Urdu',
-        'uz' => 'Uzbek',
-        've' => 'Venda',
-        'vi' => 'Vietnamese',
-        'vo' => 'VolapÃ¼k',
-        'wa' => 'Walloon',
-        'wo' => 'Wolof',
-        'xh' => 'Xhosa',
-        'yi' => 'Yiddish',
-        'yo' => 'Yoruba',
-        'za' => 'Zhuang, Chuang',
-        'zh' => 'Chinese',
-        'zu' => 'Zulu'
-        ];
-
     /**
      * Lang constructor.
      * @param $theme
@@ -240,7 +53,7 @@ class Lang
 
     public function getAllowedLanguages()
     {
-        return $this->_langs;
+        return Config::getInstance()->get('langs');
     }
 
     /**
@@ -250,6 +63,7 @@ class Lang
     public function getLangs($theme = null)
     {
         $dir = $this->dir;
+        $allowed = $this->getAllowedLanguages();
 
         if($theme){
             $dir = "themes/$theme/lang/";
@@ -262,9 +76,9 @@ class Lang
                     $entry = mb_strtolower($entry);
                     $entry = str_replace('.ini','', $entry);
 
-                    if( !isset($this->_langs[$entry])) continue;
+                    if( !isset($allowed[$entry])) continue;
 
-                    $this->langs[$entry] = $this->_langs[$entry];
+                    $this->langs[$entry] = $allowed[$entry];
                 }
             }
             closedir($handle);
@@ -301,27 +115,6 @@ class Lang
         foreach ($a as $k=>$v) {
             $this->translations[$k] = $v;
         }
-
-        /*
-        if ($handle = opendir(DOCROOT . $dir . '/' . $this->lang)) {
-            while (false !== ($entry = readdir($handle))) {
-                if ($entry != "." && $entry != ".."){
-
-                    $fn = DOCROOT . $dir . '/' . $this->lang .'/' . $entry;
-
-                    if(!file_exists($fn)) continue;
-
-                    $a = parse_ini_file($fn, true);
-
-                    foreach ($a as $k=>$v) {
-                        $this->translations[$k] = $v;
-                    }
-                }
-            }
-            closedir($handle);
-        }
-         */
-//       echo '<pre>'; print_r($this->translations);echo '</pre>';
     }
 
     /**
