4456e5262ae0fe3703d684e4221c771c1a70d7dd
Volodymyr Hodiak <wmgodyak@gmail.com>
2017-03-08 17:08:12 +0200

    changed .gitignore

---
+++ vendor/autoload.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * OYiEngine 7
+ * @author Volodymyr Hodiak mailto:support@otakoi.com
+ * @copyright Copyright (c) 2015 Otakoyi.com
+ * Date: 08.03.17 : 17:05
+ */
+
+defined("CPATH") or die();
