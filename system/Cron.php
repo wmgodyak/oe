e4240dfc8df5d5ad2fd8b9968f0c463791317f37
Volodymyr Hodiak <wmgodyak@gmail.com>
2017-05-31 15:33:13 +0300

    updated to new modules support

--- system/Cron.php
+++ system/Cron.php
@@ -7,13 +7,10 @@ use system\models\Modules;
  * Class Cron
  * @package system
  */
-class Cron// extends Front
+class Cron
 {
     public function index()
     {
-        // todo check cli
-        $m = new Modules();
-        $m->init();
         EventsHandler::getInstance()->call('system.cron.run');
     }
 
